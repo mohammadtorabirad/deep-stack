8ca262e76c21e9e5d2d645576c4d4db3f477a6a4
mohammad <m@m.com>
2025-11-27 16:16:32 +0330

    chnages in icon and some config

--- running/app/build.gradle.kts
+++ running/app/build.gradle.kts
@@ -15,6 +15,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        ndk {
+            abiFilters += listOf("armeabi-v7a", "arm64-v8a")
+        }
     }
 
     buildTypes {
